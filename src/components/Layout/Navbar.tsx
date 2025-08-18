6c81660e3e700798259e9b976d960fe953567372
Modson Tande <131668620+Modsontech45@users.noreply.github.com>
2025-08-18 01:50:11 +0000

    Updated Navbar.tsx

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -88,7 +88,7 @@ const Navbar: React.FC = () => {
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
               <input
                 type="text"
-                placeholder={t("nav.search")}
+                placeholder="Search creators, posts..."
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
                 className="w-full pl-10 pr-4 py-2 border border-gray-300 dark:border-gray-700 rounded-full bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
